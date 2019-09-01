9a6292b2c8081a67580a6401910aea1dd72e529c
ep007 <eisha.peyyeti@gmail.com>
2019-09-01 15:09:49 -0500

    newest version with gradient

--- app/src/main/java/com/example/madlibs/MadLibDisplay.java
+++ app/src/main/java/com/example/madlibs/MadLibDisplay.java
@@ -1,13 +1,16 @@
 package com.example.madlibs;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
+
 
 public class MadLibDisplay extends AppCompatActivity {
     public static final String MY_GIRL = "girl";
@@ -16,6 +19,26 @@ public class MadLibDisplay extends AppCompatActivity {
     public static final String MY_ANIMAL = "animal";
     public static final String MY_YEAR = "year";
     public static final String MY_QUOTE = "quote";
+    public static final String MY_OBJECT = "object";
+    public static final String MY_GUY = "guy";
+    public static final String MY_PLANET = "planet";
+    public static final String MY_COLOR = "color";
+    public static final String MY_TEACHER = "teacher";
+    public static final String MY_NUMBER = "number";
+
+    public static final String MY_CELEB = "celeb";
+    public static final String MY_HABIT1 = "habit1";
+    public static final String MY_HABIT2 = "habit2";
+    public static final String MY_HOLIDAY = "holiday";
+    public static final String MY_STORE = "store";
+    public static final String MY_DAY = "day";
+
+    public static final String MY_SPORT = "sport";
+    public static final String MY_TV = "tv";
+    public static final String MY_ADJ = "adj";
+    public static final String MY_COUNTRY = "country";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);
@@ -27,22 +50,41 @@ public class MadLibDisplay extends AppCompatActivity {
         String animal = intent.getStringExtra(MY_ANIMAL);
         String year = intent.getStringExtra(MY_YEAR);
         String quote = intent.getStringExtra(MY_QUOTE);
-        String strToDisplay = "Once upon a time, there was a " +
-                animal + " named " + girl + ". One day, " + girl + " went to" +
-                " Target to buy some " + food + " for her birthday party. At " +
-                "Target, " + girl + " saw her worst enemy : " + enemy + "the giraffe. Why " +
-                "were they enemies, you may ask. Well in the year " + year + ", Jennifer the Goose" +
-                " and " + girl + " were hanging out until all of a sudden, " + enemy + " came " +
-                "to their hang out sesh and started convincing Jennifer that doing drugs, vaping, " +
-                "and drinking were good ideas! " + girl + " was not okay with this and once" +
-                " Jennifer started falling on the wrong side of the tracks, " + girl + " unfriended " +
-                "her. For years, " + girl + " and Jennifer didn't speak and " + girl + " grew angry " +
-                "that " + enemy + " stole her best friend and started making her do bad things. Fast" +
-                " forward to their time at Target and " + girl + " was about to head out of the store with her" +
-                " recently purchased " + food + ". Instead of just calmly leaving " + girl + " threw the " + food +
-                " at " + enemy + " and walked out of the store like a BAWSE. Moral of the story is that: " + quote;
+        String object = intent.getStringExtra(MY_OBJECT);
+        String guy = intent.getStringExtra(MY_GUY);
+        String planet = intent.getStringExtra(MY_PLANET);
+        String color = intent.getStringExtra(MY_COLOR);
+        String teacher = intent.getStringExtra(MY_TEACHER);
+        String number = intent.getStringExtra(MY_NUMBER);
+
+        String celeb = intent.getStringExtra(MY_CELEB);
+        String habit1 = intent.getStringExtra(MY_HABIT1);
+        String habit2 = intent.getStringExtra(MY_HABIT2);
+        String holiday = intent.getStringExtra(MY_HOLIDAY);
+        String store = intent.getStringExtra(MY_STORE);
+        String day = intent.getStringExtra(MY_DAY);
+
+        String sport = intent.getStringExtra(MY_SPORT);
+        String tv = intent.getStringExtra(MY_TV);
+        String adj = intent.getStringExtra(MY_ADJ);
+        String country = intent.getStringExtra(MY_COUNTRY);
+
+        String strToDisplay = "Once upon a time, there was a " + "<b><u><i>" +
+                animal+ "</b></u></i>" + " named " + "<b><u><i>" + girl + "</b></u></i>" + ". One day, " + "<b><u><i>"+ girl + "</b></u></i>" + " went to "+ "<b><u><i>" + store+ "</b></u></i>" + " to buy some " + "<b><u><i>"+ food + "</b></u></i>"
+                + " for her " + "<b><u><i>" + holiday + "</b></u></i>" +" party. At "+ "<b><u><i>"+
+                store+ "</b></u></i>" + ", " + "<b><u><i>"+ girl +"</b></u></i>" +  " saw her worst enemy : "+ "<b><u><i>" + enemy + "</b></u></i>" + "the giraffe. Why " +
+                "were they enemies, you may ask. Well in the year "+ "<b><u><i>" + year +"</b></u></i>" +  ", " + "<b><u><i>" + teacher + "</b></u></i>" +" the Alpaca" +
+                " and "+ "<b><u><i>" + girl + "</b></u></i>" + " were hanging out until all of a sudden, " + "<b><u><i>"+ enemy + "</b></u></i>" + " came " +
+                "to their hang out sesh and started convincing "+ "<b><u><i>" + teacher + "</b></u></i>" + " that allowing their students" +
+                " to cheat on tests was a good idea! "+ "<b><u><i>" + girl + "</b></u></i>" + " was not okay with this and once " + "<b><u><i>"+ teacher +"</b></u></i>" +
+                " started falling on the wrong side of the tracks, "+ "<b><u><i>" + girl +"</b></u></i>" +  " 'unfriended' " +
+                "her. For years, "+ "<b><u><i>" + girl + "</b></u></i>" + " and " + "<b><u><i>"+ teacher +"</b></u></i>" + " didn't speak and " + "<b><u><i>"+ girl + "</b></u></i>" + " grew angry " +
+                "that "+ "<b><u><i>" + enemy +"</b></u></i>" +  " stole her best friend and started making her do bad things. Fast" +
+                " forward to their time at " + "<b><u><i>"+ store + "</b></u></i>" +" and " + "<b><u><i>"+ girl + "</b></u></i>" + " was about to head out of the store with her" +
+                " recently purchased "+ "<b><u><i>" + food +"</b></u></i>" +  ". Instead of just calmly leaving " + "<b><u><i>"+ girl +"</b></u></i>" +  " threw the " + "<b><u><i>"+ food +"</b></u></i>" +
+                " at "+ "<b><u><i>" + enemy + "</b></u></i>" + " and walked out of the store like a BAWSE. Moral of the story is that: "+ "<b><u><i>" + quote + "</b></u></i>";
         TextView str = (TextView) findViewById(R.id.info);
-        str.setText(strToDisplay);
+        str.setText(Html.fromHtml(strToDisplay));
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -54,6 +96,10 @@ public class MadLibDisplay extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void backHome2(View view) {
+        Intent home2 = new Intent(this, MainActivity.class);
+        startActivity(home2);
     }
 
 }
--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.madlibs;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConstraintLayout constraintLayout = findViewById(R.id.gradient);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4000);
+        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.start();
+
     }
 
     public void sendInfo(View v) {
@@ -23,6 +33,24 @@ public class MainActivity extends AppCompatActivity {
         EditText animal = (EditText) findViewById(R.id.animalOne);
         EditText year = (EditText) findViewById(R.id.year);
         EditText quote = (EditText) findViewById(R.id.quote);
+        EditText object = (EditText) findViewById(R.id.object);
+        EditText guy = (EditText) findViewById(R.id.guy);
+        EditText planet = (EditText) findViewById(R.id.planet);
+        EditText color = (EditText) findViewById(R.id.color);
+        EditText teacher = (EditText) findViewById(R.id.teacher);
+        EditText number = (EditText) findViewById(R.id.number);
+        EditText celebrity = (EditText) findViewById(R.id.celebrity);
+        EditText habit = (EditText) findViewById(R.id.habit);
+        EditText habit2 = (EditText) findViewById(R.id.habit2);
+        EditText holiday = (EditText) findViewById(R.id.holiday);
+        EditText store = (EditText) findViewById(R.id.store);
+        EditText day = (EditText) findViewById(R.id.day);
+        EditText sport = (EditText) findViewById(R.id.sport);
+        EditText tv = (EditText) findViewById(R.id.tv);
+        EditText adjective = (EditText) findViewById(R.id.adjective);
+        EditText country = (EditText) findViewById(R.id.country);
+
+
         // extracting the text from those edit text fields
         String myGirl = girl.getText().toString();
         String myEnemy = enemy.getText().toString();
@@ -30,6 +58,24 @@ public class MainActivity extends AppCompatActivity {
         String myAnimal = animal.getText().toString();
         String myYear = year.getText().toString();
         String myQuote = quote.getText().toString();
+        String myObject = object.getText().toString();
+        String myGuy = guy.getText().toString();
+        String myPlanet = planet.getText().toString();
+        String myColor = color.getText().toString();
+        String myTeacher = teacher.getText().toString();
+        String myNumber = number.getText().toString();
+        String myCeleb = celebrity.getText().toString();
+        String myHabitOne = habit.getText().toString();
+        String myHabitTwo = habit2.getText().toString();
+        String myHoliday = holiday.getText().toString();
+        String myStore = store.getText().toString();
+        String myDay = day.getText().toString();
+        String mySport = sport.getText().toString();
+        String myTV = tv.getText().toString();
+        String myAdj = adjective.getText().toString();
+        String myCountry = country.getText().toString();
+
+
         // Creating the intent object so I can send data
         Intent intent = new Intent(this, MadLibDisplay.class);
         // putting data from edit text fields into intent to send to other activity
@@ -40,7 +86,124 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(MadLibDisplay.MY_ANIMAL, myAnimal);
         intent.putExtra(MadLibDisplay.MY_YEAR, myYear);
         intent.putExtra(MadLibDisplay.MY_QUOTE, myQuote);
+
+        intent.putExtra(MadLibDisplay.MY_OBJECT, myObject);
+        intent.putExtra(MadLibDisplay.MY_GUY, myGuy);
+        intent.putExtra(MadLibDisplay.MY_PLANET, myPlanet);
+        intent.putExtra(MadLibDisplay.MY_COLOR, myColor);
+        intent.putExtra(MadLibDisplay.MY_TEACHER, myTeacher);
+        intent.putExtra(MadLibDisplay.MY_NUMBER, myNumber);
+
+        intent.putExtra(MadLibDisplay.MY_CELEB, myCeleb);
+        intent.putExtra(MadLibDisplay.MY_HABIT1, myHabitOne);
+        intent.putExtra(MadLibDisplay.MY_HABIT2, myHabitTwo);
+        intent.putExtra(MadLibDisplay.MY_HOLIDAY, myHoliday);
+        intent.putExtra(MadLibDisplay.MY_STORE, myStore);
+        intent.putExtra(MadLibDisplay.MY_DAY, myDay);
+
+        intent.putExtra(MadLibDisplay.MY_SPORT, mySport);
+        intent.putExtra(MadLibDisplay.MY_TV, myTV);
+        intent.putExtra(MadLibDisplay.MY_ADJ, myAdj);
+        intent.putExtra(MadLibDisplay.MY_COUNTRY, myCountry);
+
+
         // loads the next activity
         startActivity(intent);
+
     }
-}
+
+
+
+    public void sendInfo2(View v) {
+        // getting a reference to my edit text fields
+        EditText girl = (EditText) findViewById(R.id.girlOne);
+        EditText enemy = (EditText) findViewById(R.id.girlTwo);
+        EditText food = (EditText) findViewById(R.id.food);
+        EditText animal = (EditText) findViewById(R.id.animalOne);
+        EditText year = (EditText) findViewById(R.id.year);
+        EditText quote = (EditText) findViewById(R.id.quote);
+        EditText object = (EditText) findViewById(R.id.object);
+        EditText guy = (EditText) findViewById(R.id.guy);
+        EditText planet = (EditText) findViewById(R.id.planet);
+        EditText color = (EditText) findViewById(R.id.color);
+        EditText teacher = (EditText) findViewById(R.id.teacher);
+        EditText number = (EditText) findViewById(R.id.number);
+        EditText celebrity = (EditText) findViewById(R.id.celebrity);
+        EditText habit = (EditText) findViewById(R.id.habit);
+        EditText habit2 = (EditText) findViewById(R.id.habit2);
+        EditText holiday = (EditText) findViewById(R.id.holiday);
+        EditText store = (EditText) findViewById(R.id.store);
+        EditText day = (EditText) findViewById(R.id.day);
+        EditText sport = (EditText) findViewById(R.id.sport);
+        EditText tv = (EditText) findViewById(R.id.tv);
+        EditText adjective = (EditText) findViewById(R.id.adjective);
+        EditText country = (EditText) findViewById(R.id.country);
+
+
+
+
+
+        // extracting the text from those edit text fields
+        String myGirl = girl.getText().toString();
+        String myEnemy = enemy.getText().toString();
+        String myFood = food.getText().toString();
+        String myAnimal = animal.getText().toString();
+        String myYear = year.getText().toString();
+        String myQuote = quote.getText().toString();
+        String myObject = object.getText().toString();
+        String myGuy = guy.getText().toString();
+        String myPlanet = planet.getText().toString();
+        String myColor = color.getText().toString();
+        String myTeacher = teacher.getText().toString();
+        String myNumber = number.getText().toString();
+        String myCeleb = celebrity.getText().toString();
+        String myHabitOne = habit.getText().toString();
+        String myHabitTwo = habit2.getText().toString();
+        String myHoliday = holiday.getText().toString();
+        String myStore = store.getText().toString();
+        String myDay = day.getText().toString();
+        String mySport = sport.getText().toString();
+        String myTV = tv.getText().toString();
+        String myAdj = adjective.getText().toString();
+        String myCountry = country.getText().toString();
+
+
+
+        // Creating the intent object so I can send data
+
+        Intent intent2 =  new Intent( this, MadLibDisplay2.class);
+        intent2.putExtra(MadLibDisplay.MY_GIRL, myGirl);
+        intent2.putExtra(MadLibDisplay.MY_ENEMY, myEnemy);
+        intent2.putExtra(MadLibDisplay.MY_FOOD, myFood);
+        intent2.putExtra(MadLibDisplay.MY_ANIMAL, myAnimal);
+        intent2.putExtra(MadLibDisplay.MY_YEAR, myYear);
+        intent2.putExtra(MadLibDisplay.MY_QUOTE, myQuote);
+
+        intent2.putExtra(MadLibDisplay.MY_OBJECT, myObject);
+        intent2.putExtra(MadLibDisplay.MY_GUY, myGuy);
+        intent2.putExtra(MadLibDisplay.MY_PLANET, myPlanet);
+        intent2.putExtra(MadLibDisplay.MY_COLOR, myColor);
+        intent2.putExtra(MadLibDisplay.MY_TEACHER, myTeacher);
+        intent2.putExtra(MadLibDisplay.MY_NUMBER, myNumber);
+
+        intent2.putExtra(MadLibDisplay.MY_CELEB, myCeleb);
+        intent2.putExtra(MadLibDisplay.MY_HABIT1, myHabitOne);
+        intent2.putExtra(MadLibDisplay.MY_HABIT2, myHabitTwo);
+        intent2.putExtra(MadLibDisplay.MY_HOLIDAY, myHoliday);
+        intent2.putExtra(MadLibDisplay.MY_STORE, myStore);
+        intent2.putExtra(MadLibDisplay.MY_DAY, myDay);
+
+        intent2.putExtra(MadLibDisplay.MY_SPORT, mySport);
+        intent2.putExtra(MadLibDisplay.MY_TV, myTV);
+        intent2.putExtra(MadLibDisplay.MY_ADJ, myAdj);
+        intent2.putExtra(MadLibDisplay.MY_COUNTRY, myCountry);
+
+        startActivity(intent2);
+    }
+
+    public void backHome(View v){
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+    }
+    }
+
